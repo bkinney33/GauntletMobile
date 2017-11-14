ea98d8be4d663d89dcfcaf1530c89c259dfba632
Denver <dragonflamr@gmail.com>
2017-11-14 10:10:08 -0700

    added correct positioning for scripts

---
+++ GauntletMobile/Assets/Scripts/PlayerScripts/ResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour {
+
+    [SerializeField]
+    UnityEngine.UI.Image healthBar;
+    [SerializeField]
+    UnityEngine.UI.Image manaBar;
+
+    public void UpdateHealth(double percent)
+    {
+        healthBar.fillAmount = (float)percent;
+    }
+    public void UpdateMana(double percent)
+    {
+        manaBar.fillAmount = (float)percent;
+    }
+
+
+}
