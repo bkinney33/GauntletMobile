efed6bdd911adf093f3bfbe41346e3cb04b8cd71
bkinney33 <kinneybrennan33@gmail.com>
2017-11-17 11:35:37 -0700

    added sprites and rotations

--- GauntletMobile/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ GauntletMobile/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -8,10 +8,15 @@ public class PlayerMovement : MonoBehaviour {
     [Tooltip("How Fast the Player Moves.")]
     [SerializeField] float playerSpeed = 3f;
     Rigidbody2D rigidBody;
+    Transform body;
+    Component transform;
 
     private void Start()
     {
+        body = gameObject.GetComponent<Transform>();
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        transform = gameObject.transform.Find("Body");
+
     }
 
     // Update is called once per frame
@@ -23,9 +28,18 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Movement()
     {
+        float currentX = body.position.x;
+        float currentY = body.position.y;
+   
         float h = playerSpeed * CrossPlatformInputManager.GetAxis("Horizontal");
         float v = playerSpeed * CrossPlatformInputManager.GetAxis("Vertical");
         rigidBody.velocity = (new Vector2(h, v));
-        
-    }
+        float heading = Mathf.Atan2(CrossPlatformInputManager.GetAxis("Horizontal"),-CrossPlatformInputManager.GetAxis("Vertical"));
+       
+        transform.transform.rotation = Quaternion.Euler(0f, 0f, (((heading) * Mathf.Rad2Deg)));
+      
+   
+
+
+}
 }
