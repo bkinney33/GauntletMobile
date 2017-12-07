bc6b9a7702e06cbd847b69901255c34c788004f2
Denver <dragonflamr@gmail.com>
2017-12-07 10:05:42 -0700

    some small polish

--- GauntletMobile/Assets/Scripts/ChangeSceneOnCollision.cs
+++ GauntletMobile/Assets/Scripts/ChangeSceneOnCollision.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,6 +10,11 @@ public class ChangeSceneOnCollision : MonoBehaviour {
     string newSceneName;
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        LoadScene();
+    }
+
+    public void LoadScene()
     {
         SceneManager.LoadScene(newSceneName);
     }
